fc93986ce9ef0c30ef687fd07f7457722e598442
DESKTOP-QA5O2KU\samsung <hshlego@naver.com>
2022-05-04 19:42:31 +0900

    백테스트 수정

--- backtest.py
+++ backtest.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def backtest(df: pd.DataFrame, budget: float, start: int, length: int, init_method, method) -> float:
+def backtest(df: pd.DataFrame, budget: float, start: int, length: int, init_method, method, fee) -> float:
     transactions_df = init_method(df, start, length)
 
     asset = 0  # 코인
@@ -9,7 +9,7 @@ def backtest(df: pd.DataFrame, budget: float, start: int, length: int, init_meth
     bought = False
 
     for index, row in transactions_df.iterrows():
-        asset, budget, bought = method(row, asset, budget, bought)
+        asset, budget, bought = method(row, asset, budget, bought, fee)
 
     # 마지막에 들고 있는 돈 = 현재 종가 * 코인 량 + 현금
     end_budget = df['close'].iloc[len(transactions_df)] * asset + budget
