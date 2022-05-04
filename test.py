d07da916f2d0f4c36f2feed6bd51e19abb00bedf
DESKTOP-QA5O2KU\samsung <hshlego@naver.com>
2022-05-04 19:46:05 +0900

    테스트 : sma, wma, ema, 존버

--- test.py
+++ test.py
@@ -1,17 +1,20 @@
-import parser
+import csv_parser
 from backtest import backtest
 import strategy
 
-btc = parser.get_dataframe()
+btc = csv_parser.get_dataframe()
 budget = 1_000_000
 start = 0
 days = 7
-
-sma_profit = backtest(btc, 1_000_000, start, days*24, strategy.init_simple_moving_average, strategy.simple_moving_average)
+fee = 0.00
+sma_profit = backtest(btc, 1_000_000, start, days*24, strategy.init_simple_moving_average, strategy.simple_moving_average, fee)
 print("sma profit :", sma_profit)
 
-wma_profit = backtest(btc, 1_000_000, start, days*24, strategy.init_weighted_moving_average, strategy.weighted_moving_average)
+wma_profit = backtest(btc, 1_000_000, start, days*24, strategy.init_weighted_moving_average, strategy.weighted_moving_average, fee)
 print("wma profit :", wma_profit)
 
-ema_profit = backtest(btc, 1_000_000, start, days*24, strategy.init_exponential_moving_average, strategy.exponential_moving_average)
+ema_profit = backtest(btc, 1_000_000, start, days*24, strategy.init_exponential_moving_average, strategy.exponential_moving_average, fee)
 print("ema profit :", ema_profit)
+
+johnbur_profit = backtest(btc, 1_000_000, start, days*24, strategy.init_johnbur, strategy.johnbur, 0.05)
+print("존버 profit :", johnbur_profit)
