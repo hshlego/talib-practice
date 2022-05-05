d1e62da6e2f70f4e4f215c5d9ac26627460923fb
hwan <hshlego@naver.com>
2022-05-05 09:41:20 +0900

    binance 수수료 추가

--- test.py
+++ test.py
@@ -2,11 +2,17 @@ import csv_parser
 from backtest import backtest
 import strategy
 
+fees = {
+    "upbit": 0.05,
+    "binance": 0.1
+}
+
 btc = csv_parser.get_dataframe()
 budget = 1_000_000
 start = 0
-days = 7
-fee = 0.00
+days = 180
+fee = fees["binance"]*0.01
+
 sma_profit = backtest(btc, 1_000_000, start, days*24, strategy.init_simple_moving_average, strategy.simple_moving_average, fee)
 print("sma profit :", sma_profit)
 
@@ -16,5 +22,5 @@ print("wma profit :", wma_profit)
 ema_profit = backtest(btc, 1_000_000, start, days*24, strategy.init_exponential_moving_average, strategy.exponential_moving_average, fee)
 print("ema profit :", ema_profit)
 
-johnbur_profit = backtest(btc, 1_000_000, start, days*24, strategy.init_johnbur, strategy.johnbur, 0.05)
+johnbur_profit = backtest(btc, 1_000_000, start, days*24, strategy.init_johnbur, strategy.johnbur, fee)
 print("존버 profit :", johnbur_profit)
