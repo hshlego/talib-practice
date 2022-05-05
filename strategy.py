68dfe74aa312bc0cc13ceaaa5b70931f88962626
hwan <hshlego@naver.com>
2022-05-05 09:38:51 +0900

    종가(close) 역순 저장

--- strategy.py
+++ strategy.py
@@ -7,11 +7,11 @@ def init_simple_moving_average(df: pd.DataFrame, start: int, length: int) -> pd.
     close_list = csv_parser.get_nparray(df, 'close', start, length)
 
     transactions_df = pd.DataFrame()
-    transactions_df['close'] = df['close'].iloc[start:length]  # 종가를 범위만큼 불러와서 데이터프레임에 저장
+    transactions_df['close'] = df['close'].iloc[start:start+length].iloc[::-1]  # 종가를 범위만큼 불러와서 데이터프레임에 저장
     transactions_df['sma5'] = talib.SMA(close_list, 5)  # 5일선
     transactions_df['sma20'] = talib.SMA(close_list, 20)  # 20일선
     transactions_df.dropna(inplace=True)  # na값 드랍
-    return transactions_df[::-1]
+    return transactions_df
 
 
 def simple_moving_average(series: pd.Series, asset, budget, bought, fee) -> (int, int, bool):
@@ -34,11 +34,11 @@ def init_weighted_moving_average(df: pd.DataFrame, start: int, length: int) -> p
     close_list = csv_parser.get_nparray(df, 'close', start, length)
 
     transactions_df = pd.DataFrame()
-    transactions_df['close'] = df['close'].iloc[start:length]
+    transactions_df['close'] = df['close'].iloc[start:start+length].iloc[::-1]
     transactions_df['wma5'] = talib.WMA(close_list, 5)
     transactions_df['wma20'] = talib.WMA(close_list, 20)
     transactions_df.dropna(inplace=True)
-    return transactions_df[::-1]
+    return transactions_df
 
 
 def weighted_moving_average(series: pd.Series, asset, budget, bought, fee) -> (int, int, bool):
@@ -61,11 +61,11 @@ def init_exponential_moving_average(df: pd.DataFrame, start: int, length: int) -
     close_list = csv_parser.get_nparray(df, 'close', start, length)
 
     transactions_df = pd.DataFrame()
-    transactions_df['close'] = df['close'].iloc[start:length]
+    transactions_df['close'] = df['close'].iloc[start:start+length].iloc[::-1]
     transactions_df['ema5'] = talib.EMA(close_list, 5)
     transactions_df['ema20'] = talib.EMA(close_list, 20)
     transactions_df.dropna(inplace=True)
-    return transactions_df[::-1]
+    return transactions_df
 
 
 def exponential_moving_average(series: pd.Series, asset, budget, bought, fee) -> (int, int, bool):
@@ -88,8 +88,8 @@ def init_johnbur(df: pd.DataFrame, start: int, length: int) -> pd.DataFrame:
     close_list = csv_parser.get_nparray(df, 'close', start, length)
 
     transactions_df = pd.DataFrame()
-    transactions_df['close'] = df['close'].iloc[start:length]
-    return transactions_df[::-1]
+    transactions_df['close'] = df['close'].iloc[start:start+length].iloc[::-1]
+    return transactions_df
 
 
 def johnbur(series: pd.Series, asset, budget, bought, fee) -> (int, int, bool):
