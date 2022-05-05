6426c8e663c0e952f7c2e5be3545f7d21d6b3f74
hwan <hshlego@naver.com>
2022-05-05 23:05:38 +0900

    get_all_coin_dataframe 수정

--- csv_parser.py
+++ csv_parser.py
@@ -13,7 +13,7 @@ def get_all_coin_dataframe(path="./csv_files") -> list:
     coin_list = ["BTC", "ETH", "ADA", "XRP", "DOGE"]
     coin_df_list = []
     for coin in coin_list:
-        file_path = path + "/Binance_" + str(coin) + "USDT_1h.csv",
+        file_path = path + "/Binance_" + str(coin) + "USDT_1h.csv"
         df = pd.read_csv(file_path, header=1)
         coin_df_list.append({"name": coin, "dataframe": df})
 
