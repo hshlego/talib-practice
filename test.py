59c08ac1a439e2c3d16c9e1b6e3cfb62daac2efc
Kanguk Lee <p51lee@kaist.ac.kr>
2022-05-05 18:45:00 +0900

    Format output

--- test.py
+++ test.py
@@ -10,29 +10,29 @@ fees = {
 btc = csv_parser.get_dataframe()
 budget = 1_000_000
 start = 0
-days = 360
+days = 30
 fee = fees["binance"]*0.01
 
 sma_profit = backtest(btc, 1_000_000, start, days*24, strategy.init_simple_moving_average, strategy.simple_moving_average, fee)
-print("sma profit :", sma_profit)
+print("sma profit : {:.3f}".format(sma_profit))
 
 wma_profit = backtest(btc, 1_000_000, start, days*24, strategy.init_weighted_moving_average, strategy.weighted_moving_average, fee)
-print("wma profit :", wma_profit)
+print("wma profit : {:.3f}".format(wma_profit))
 
 ema_profit = backtest(btc, 1_000_000, start, days*24, strategy.init_exponential_moving_average, strategy.exponential_moving_average, fee)
-print("ema profit :", ema_profit)
+print("ema profit : {:.3f}".format(ema_profit))
 
 bbd_profit = backtest(btc, 1_000_000, start, days*24, strategy.init_bollinger_bands, strategy.bollinger_bands, fee)
-print("bbd profit :", bbd_profit)
+print("bbd profit : {:.3f}".format(bbd_profit))
 
 env_profit = backtest(btc, 1_000_000, start, days*24, strategy.init_envelope, strategy.envelope, fee)
-print("env profit :", env_profit)
+print("env profit : {:.3f}".format(env_profit))
 
 rsi_profit = backtest(btc, 1_000_000, start, days*24, strategy.init_rsi, strategy.rsi, fee)
-print("rsi profit :", rsi_profit)
+print("rsi profit : {:.3f}".format(rsi_profit))
 
 cci_profit = backtest(btc, 1_000_000, start, days*24, strategy.init_cci, strategy.cci, fee)
-print("cci profit :", cci_profit)
+print("cci profit : {:.3f}".format(cci_profit))
 
 johnbur_profit = backtest(btc, 1_000_000, start, days*24, strategy.init_johnbur, strategy.johnbur, fee)
-print("존버 profit :", johnbur_profit)
+print("jbr profit : {:.3f}".format(johnbur_profit))
